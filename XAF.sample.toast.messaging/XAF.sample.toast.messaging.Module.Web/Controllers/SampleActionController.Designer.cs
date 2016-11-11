c1c8bf52d0dc91ad1a80c4bfb7dab91fad74afe4
wolfhermann <wolf.hermann@nit-consultants.de>
2016-11-11 15:55:24 +0100

    First draft

---
+++ XAF.sample.toast.messaging/XAF.sample.toast.messaging.Module.Web/Controllers/SampleActionController.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace XAF.sample.toast.messaging.Module.Web.Controllers
+{
+    partial class SampleActionController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleAction1 = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // simpleAction1
+            // 
+            this.simpleAction1.Caption = "Show some message";
+            this.simpleAction1.ConfirmationMessage = null;
+            this.simpleAction1.Id = "25b13bf5-b738-4db7-b9b9-c0550be649f2";
+            this.simpleAction1.ToolTip = null;
+            this.simpleAction1.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleAction1_Execute);
+            // 
+            // SampleActionController
+            // 
+            this.Actions.Add(this.simpleAction1);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleAction1;
+    }
+}
---
+++ XAF.sample.toast.messaging/XAF.sample.toast.messaging.Module.Web/WebModule.Designer.cs
@@ -0,0 +1,35 @@
+﻿namespace XAF.sample.toast.messaging.Module.Web {
+    partial class messagingAspNetModule {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            // 
+            // messagingAspNetModule
+            // 
+            this.RequiredModuleTypes.Add(typeof(XAF.sample.toast.messaging.Module.messagingModule));
+            this.RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule));
+        }
+
+        #endregion
+    }
+}
